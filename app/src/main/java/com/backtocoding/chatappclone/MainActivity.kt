9ba0bbba013fb95741e4c1d0f96d8d1912bd6b51
MilindAmrutkar <milind27.amrutkar@gmail.com>
2023-07-16 10:25:26 +0530

    Added: NavHost and Controller

--- app/src/main/java/com/backtocoding/chatappclone/MainActivity.kt
+++ app/src/main/java/com/backtocoding/chatappclone/MainActivity.kt
@@ -6,8 +6,33 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.backtocoding.chatappclone.ui.ChatListScreen
+import com.backtocoding.chatappclone.ui.ProfileScreen
+import com.backtocoding.chatappclone.ui.SignupScreen
+import com.backtocoding.chatappclone.ui.SingleChatScreen
+import com.backtocoding.chatappclone.ui.SingleStatusScreen
+import com.backtocoding.chatappclone.ui.StatusListScreen
 import com.backtocoding.chatappclone.ui.theme.ChatAppCloneTheme
+
+sealed class DestinationScreen(val route: String) {
+    object Signup : DestinationScreen("signup")
+    object Login : DestinationScreen("login")
+    object Profile : DestinationScreen("profile")
+    object ChatList : DestinationScreen("chatList")
+    object SingleChat : DestinationScreen("singleChat/{chatId}") {
+        fun createRoute(id: String) = "singleChat/$id"
+    }
+
+    object StatusList : DestinationScreen("statusList")
+    object SingleStatus : DestinationScreen("singleStatus/{statusId}") {
+        fun createRoute(id: String) = "singleStatus/$id"
+    }
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,9 +44,37 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    LoginScreen()
+                    ChatAppNavigation()
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun ChatAppNavigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = DestinationScreen.Signup.route) {
+        composable(DestinationScreen.Signup.route) {
+            SignupScreen()
+        }
+        composable(DestinationScreen.Login.route) {
+            LoginScreen()
+        }
+        composable(DestinationScreen.Profile.route) {
+            ProfileScreen()
+        }
+        composable(DestinationScreen.StatusList.route) {
+            StatusListScreen()
+        }
+        composable(DestinationScreen.SingleStatus.route) {
+            SingleStatusScreen(statusId = "123")
+        }
+        composable(DestinationScreen.ChatList.route) {
+            ChatListScreen()
+        }
+        composable(DestinationScreen.SingleChat.route) {
+            SingleChatScreen(chatId = "123")
         }
     }
 }
